c7cbdf24a3fb9ac43b48e6968c1cd7ad82411104
Rene Reimann <rene@die-pixler.de>
2016-01-28 12:06:34 +0100

    add credit Page

--- src/inc/templates/credits.php
+++ src/inc/templates/credits.php
@@ -19,5 +19,24 @@ if ( ! defined( 'INSR_DIR' ) ) {
 		<a class="nav-tab nav-tab-active" href="<?php echo admin_url() ?>tools.php?page=credits"><?php esc_html_e( 'Credits', 'insr' ); ?></a>
 	</h2>
 
-	<p><?php esc_html_e( 'The Inpsyde Credits.', 'insr' ); ?> </p>
+	<h2><?php _e( "Hey nice to have you here!", 'insr' ); ?></h2>
+	<p><?php _e( 'Search and Replace is refactored by <a href="http://inpsyde.com/en/">Inpsyde GmbH</a> and based on the original from <a href="http://thedeadone.net">Mark Cunningham</a> ', 'insr' ); ?> </p>
 
+	<h2><?php _e( "We are Inpsyde", 'insr' ); ?></h2>
+	<p><?php _e( "Inpsyde has developed enterprise solutions with the world's most popular open-source CMS since it was a kitten. Still do, inconvincibly convinced.", 'insr' ); ?> </p>
+	<p><?php _e( "Inpsyde is a WordPress <a href='https://vip.wordpress.com/partner/inpsyde/'>VIP Service Partner</a> and <a href='https://www.woothemes.com/experts/inpsyde-gmbh/'>WooCommerce Experten</a>.", 'insr' ); ?> </p>
+	<p><?php _e( "<a href='https://profiles.wordpress.org/inpsyde/#content-plugins'>Look at our other WordPress plugins</a>", 'insr' ); ?> </p>
+
+
+	<h2><?php _e( "Working at Inpsyde", 'insr' ); ?></h2>
+	<p><?php _e( "s the biggest WordPress enterprise in Europe we’re dynamically growing and constantly looking for new employees. So do you want to shape WordPress in an interesting and exciting working environment? Here we are!", 'insr' ); ?> </p>
+	<p><?php _e( "At the moment we’re looking for developers for WordPress based products and services. If you’re not a developer and want to be part of us, we’d be happy to recieve your unsolicited application. At Inpsyde you can expect an open, modern and lively company culture:", 'insr' ); ?> </p>
+	<ol>
+		<li><?php _e( "challenging and exciting projects", 'insr' ); ?></li>
+		<li><?php _e( "flexible working hours in remote office", 'insr' ); ?></li>
+		<li><?php _e( "deliberately flat hierarchies and short decision paths", 'insr' ); ?></li>
+		<li><?php _e( "a wide variety of tasks", 'insr' ); ?></li>
+		<li><?php _e( "freedom for personal development and responsible, self-reliant action", 'insr' ); ?></li>
+
+	</ol>
+	<p><?php _e( "If you love open source and especially WordPress, if you love to organize your working days by yourself and want to use your pragmatic problem-solving skills and result-oriented work methods: <a href='mailto:jobs@inpsyde.com'>join our team</a>!", 'insr' ); ?> </p>
