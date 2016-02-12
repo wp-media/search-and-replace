f1e4ae0220b2ece9fb709fca3aa634d973495d46
Rene Reimann <rene@die-pixler.de>
2016-02-12 14:58:56 +0100

    change text-domain #47

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,21 +28,21 @@ module.exports = function(grunt) {
 		makepot: {
 			target: {
 				options: {
-					cwd: '',                          // Directory of files to internationalize.
-					domainPath: 'src/languages/',               // Where to save the POT file.
-					exclude: [],                      // List of files or directories to ignore.
-					include: ['src/.*'],                      // List of files or directories to include.
-					mainFile: 'inspyde-search-replace.php',                     // Main project file.
-					potComments: '',                  // The copyright at the beginning of the POT file.
-					potFilename: 'insr.pot',              // Name of the POT file.
+					cwd: '',                                // Directory of files to internationalize.
+					domainPath: 'src/languages/',           // Where to save the POT file.
+					exclude: [],                            // List of files or directories to ignore.
+					include: ['src/.*'],                    // List of files or directories to include.
+					mainFile: 'inspyde-search-replace.php', // Main project file.
+					potComments: '',                        // The copyright at the beginning of the POT file.
+					potFilename: 'search-and-replace.pot',  // Name of the POT file.
 					potHeaders: {
-						poedit: false,                 // Includes common Poedit headers.
-						'x-poedit-keywordslist': true // Include a list of all possible gettext functions.
-					},                                // Headers to add to the generated POT file.
-					processPot: null,                 // A callback function for manipulating the POT file.
-					type: 'wp-plugin',                // Type of project (wp-plugin or wp-theme).
-					updateTimestamp: true,            // Whether the POT-Creation-Date should be updated without other changes.
-					updatePoFiles: false              // Whether to update PO files in the same directory as the POT file.
+						poedit: false,                      // Includes common Poedit headers.
+						'x-poedit-keywordslist': true       // Include a list of all possible gettext functions.
+					},                                      // Headers to add to the generated POT file.
+					processPot: null,                       // A callback function for manipulating the POT file.
+					type: 'wp-plugin',                      // Type of project (wp-plugin or wp-theme).
+					updateTimestamp: true,                  // Whether the POT-Creation-Date should be updated without other changes.
+					updatePoFiles: false                    // Whether to update PO files in the same directory as the POT file.
 				}
 			}
 		}
