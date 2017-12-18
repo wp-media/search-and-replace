8d4ed497e002e6ffff71df512b7b853cc6d99ad9
Guido Scialfa <dev@guidoscialfa.com>
2017-12-18 14:10:48 +0100

    Update ReplaceTest
    Fixing little issues with the serialized data replacing.
    More info:
    
    The logic however is right, the recursive function try to unpack
    the data until it's get a simple string value, make the replace and return up.
    
    The issue is in the order we pass the `$serialised` parameter.
    Now every recursive call to `recursive_unserialize_replace`
    use always `false`, `true` must be set when we call the function outside,
    so we can retrieve the serialized value "correctly replaced" (tests permits)
    internally we try to unserialize the data the first time,
    then when every call return up to the caller we have the first `$serialised`
    set to `true` and the data will be serialized accordingly.
    
    I've only some doubts about the use of `is_serialized`, `is_serialized_string` and the use of `unserialize`, `serialize`.
    
    There was a comment within `recursive_unserialize_replace` that tell to switch to
    `maybe_unserialize` because serialized data by WordPress is different
    than the serialized data by `serialize` but I'm not sure,
    WordPress internally use `serialize` (in wrong way I think because permit
    object serialization and deserialization aka object injection but since we should work with it.
    
    Also, right know I put the same code used in WordPress for:
     - `is_serialized_string`,
     - `is_serialized`
     - `maybe_serialize`
     because any other stub will not well defined as WordPress do and since
     serializing data is a sensitive data I think in this case better to use
     the original functions as if it were native functions.
    
     Also, removed `add_error` undefined method call and replaced with
     a throw for when `WP_DEBUG` is active and an action when not.
     See catch block.

--- inc/Database/Replace.php
+++ inc/Database/Replace.php
@@ -52,7 +52,7 @@ class Replace {
 	 *
 	 * @var array
 	 */
-	private $csv_data = array();
+	private $csv_data = [];
 
 	/**
 	 * Replace constructor.
@@ -73,7 +73,7 @@ class Replace {
 	 * We split large tables into  blocks (size is set via $page_size)when dealing with them to save
 	 * on memory consumption.
 	 *
-	 * @param string $search  What we want to replace
+	 * @param string $search  What we want to replace.
 	 * @param string $replace What we want to replace it with.
 	 * @param string $tables  The name of the table we want to look at.
 	 * @param null   $csv
@@ -89,12 +89,12 @@ class Replace {
 
 		$this->max_execution->set();
 
-		$report = array(
+		$report = [
 			'errors'        => null,
-			'changes'       => array(),
+			'changes'       => [],
 			'tables'        => '0',
 			'changes_count' => '0',
-		);
+		];
 
 		foreach ( (array) $tables as $table ) {
 			//count tables
@@ -125,16 +125,23 @@ class Replace {
 	 */
 	public function replace_values( $search = '', $replace = '', $table, $csv = null ) {
 
-		$table_report = array(
+		$table_report = [
 			'table_name' => $table,
 			'rows'       => 0,
 			'change'     => 0,
-			'changes'    => array(),
+			'changes'    => [],
 			'updates'    => 0,
 			'start'      => microtime(),
 			'end'        => microtime(),
-			'errors'     => array(),
-		);
+			'errors'     => [],
+		];
+
+		// Check we have a search string, bail if not.
+		if ( empty( $search ) && empty( $csv ) ) {
+			$table_report[ 'errors' ][] = 'Search string is empty';
+
+			return $table_report;
+		}
 
 		// Grab table structure in order to determine which columns are used to store serialized values in it.
 		$table_structure = $this->dbm->get_table_structure( $table );
@@ -143,22 +150,19 @@ class Replace {
 			return $table_report;
 		}
 
-		$maybe_serialized = array();
+		$maybe_serialized = [];
 		foreach ( $table_structure as $struct ) {
 			// Longtext is used for meta_values as best practice in all of the automatic products.
+			// @codingStandardsIgnoreLine
 			if ( 0 === stripos( $struct->Type, 'longtext' ) ) {
+				// @codingStandardsIgnoreLine
 				$maybe_serialized[] = strtolower( $struct->Field );
 			}
 		}
 
-		// check we have a search string, bail if not
-		if ( empty( $search ) && empty( $csv ) ) {
-			$table_report[ 'errors' ][] = 'Search string is empty';
-
-			return $table_report;
-		}
-		//split columns array in primary key string and columns array
+		// Split columns array in primary key string and columns array.
 		$columns = $this->dbm->get_columns( $table );
+
 		list( $primary_key, $columns ) = $columns;
 
 		if ( null === $primary_key ) {
@@ -180,7 +184,7 @@ class Replace {
 		$pages     = ceil( $row_count / $page_size );
 
 		// Prepare CSV data
-		if ( $csv !== null ) {
+		if ( null !== $csv ) {
 			$csv_lines = explode( "\n", $csv );
 			$csv_head  = str_getcsv( 'search,replace' );
 			foreach ( $csv_lines as $line ) {
@@ -200,18 +204,18 @@ class Replace {
 			}
 
 			foreach ( $data as $row ) {
+				++ $table_report[ 'rows' ];
 
-				$table_report[ 'rows' ] ++;
-
-				$update_sql = array();
-				$where_sql  = array();
-				$update     = false;
+				$update_sql = [];
+				$where_sql  = [];
+				$update     = true;
 
 				foreach ( $columns as $column ) {
-					//Skip the GUID column per Wordpress Codex
+					// Skip the GUID column per WordPress Codex.
 					if ( $column === 'guid' ) {
 						continue;
 					}
+
 					$data_to_fix = $row[ $column ];
 
 					if ( $column === $primary_key ) {
@@ -222,62 +226,65 @@ class Replace {
 					// Run a search replace on the data that'll respect the serialisation.
 					if ( in_array( strtolower( $column ), $maybe_serialized, true ) ) {
 						// Run a search replace on the data that'll respect the serialisation.
-						$edited_data = $this->recursive_unserialize_replace( $search, $replace, $data_to_fix );
+						$edited_data = $this->recursive_unserialize_replace( $search, $replace, $data_to_fix, true );
 					} else {
 						$edited_data = str_replace( $search, $replace, $data_to_fix );
 					}
 
 					// Run a search replace by CSV parameters if CSV input present
-					if ( $csv !== null ) {
+					if ( null !== $csv ) {
 						foreach ( $this->csv_data as $entry ) {
-							$edited_data = $this->recursive_unserialize_replace( $entry[ 'search' ],
-								$entry[ 'replace' ], $edited_data );
+							$edited_data = $this->recursive_unserialize_replace(
+								$entry[ 'search' ],
+								$entry[ 'replace' ],
+								$edited_data
+							);
 						}
 					}
 
-					// Something was changed
+					// Something was changed.
 					if ( $edited_data !== $data_to_fix ) {
-
-						$table_report[ 'change' ] ++;
+						++ $table_report[ 'change' ];
 
 						// log changes
-						//TODO : does it work with non UTF-8 encodings?
-						$table_report[ 'changes' ][] = array(
+						// @todo : does it work with non UTF-8 encodings?
+						$table_report[ 'changes' ][] = [
 							'row'    => $table_report[ 'rows' ],
 							'column' => $column,
 							'from'   => $data_to_fix,
 							'to'     => $edited_data,
-						);
+						];
 
 						$update_sql[] = $column . ' = "' . $this->mysql_escape_mimic( $edited_data ) . '"';
 						$update       = true;
-
 					}
-
 				}
 
 				// Determine what to do with updates.
 				if ( true === $this->dry_run ) {
-					// Don't do anything if a dry run
-				} elseif ( $update && ! empty( $where_sql ) ) {
-					// If there are changes to make, run the query.
+					// Don't do anything if a dry run.
+					continue;
+				}
 
+				if ( $update && ! empty( $where_sql ) ) {
+					// If there are changes to make, run the query.
 					$result = $this->dbm->update( $table, $update_sql, $where_sql );
 
 					if ( ! $result ) {
 						$table_report[ 'errors' ][] = sprintf(
-							__( 'Error updating row: %d.', 'search-and-replace' ),
+						/* translators: $1 is the number of rows found in database */
+							esc_html__( 'Error updating row: %d.', 'search-and-replace' ),
 							$row
 						);
 					} else {
 						$table_report[ 'updates' ] ++;
 					}
-
 				}
 			}
 		}
 
 		$table_report[ 'end' ] = microtime( true );
+
 		$this->dbm->flush();
 
 		return $table_report;
@@ -285,31 +292,40 @@ class Replace {
 	}
 
 	/**
+	 * Recursive unserialize replace
+	 *
 	 * Take a serialised array and unserialize it replacing elements as needed and
 	 * unserializing any subordinate arrays and performing the replace on those too.
 	 *
 	 * @param string              $from       String we're looking to replace.
-	 * @param string              $to         What we want it to be replaced with
+	 * @param string              $to         What we want it to be replaced with.
 	 * @param array|string|object $data       Used to pass any subordinate arrays back to in.
 	 * @param bool                $serialised Does the array passed via $data need serialising.
+	 *
+	 * @throws \Throwable Whatever exception is thrown if WP_DEBUG is true.
 	 *
 	 * @return array The original array with all elements replaced as needed.
 	 */
 	public function recursive_unserialize_replace( $from = '', $to = '', $data = '', $serialised = false ) {
 
-		// some unserialized data cannot be re-serialised eg. SimpleXMLElements
+		// Some unserialized data cannot be re-serialised eg. SimpleXMLElements.
 		try {
+			$unserialized = ( is_serialized( $data ) ) ?
+				// @codingStandardsIgnoreLine
+				unserialize( $data, [] ) :
+				false;
 
-			if ( is_string( $data ) && is_serialized( $data ) && ( $unserialized = @unserialize( $data, false ) ) !== false ) {
+			if ( is_string( $data ) && is_serialized( $data ) && $unserialized !== false ) {
 				// Changed to maybe_unserialize because wp serialization != php serialization.
-				$data = $this->recursive_unserialize_replace( $from, $to, $unserialized, true );
+				$data = $this->recursive_unserialize_replace( $from, $to, $unserialized );
 			} elseif ( is_array( $data ) ) {
-				$_tmp = array();
+				$_tmp = [];
 				foreach ( (array) $data as $key => $value ) {
-					$_tmp[ $key ] = $this->recursive_unserialize_replace( $from, $to, $value, false );
+					$_tmp[ $key ] = $this->recursive_unserialize_replace( $from, $to, $value );
 				}
 
 				$data = $_tmp;
+
 				unset( $_tmp );
 			} elseif ( is_object( $data ) ) {
 				$_tmp  = $data;
@@ -319,13 +335,14 @@ class Replace {
 				}
 
 				$data = $_tmp;
+
 				unset( $_tmp );
 			} else {
 				if ( is_string( $data ) ) {
-
 					$marker = false;
 					if ( is_serialized_string( $data ) ) {
-						$data   = @unserialize( $data );
+						// @codingStandardsIgnoreLine
+						$data   = unserialize( $data, [] );
 						$marker = true;
 					}
 
@@ -333,6 +350,7 @@ class Replace {
 					// If after replacement data is_serialized then add one | to the replacement.
 					$tmp_data = $data;
 					$data     = str_replace( $from, $to, $data );
+
 					if ( is_serialized( $data, false ) ) {
 						$data = str_replace( $from, '|' . $to, $tmp_data );
 					}
@@ -340,21 +358,23 @@ class Replace {
 					if ( $marker ) {
 						$data = maybe_serialize( $data );
 					}
-
 				} else {
 					$data = str_replace( $from, $to, $data );
+					// @codingStandardsIgnoreLine
 					$data = serialize( $data );
 				}
 			}
 
 			if ( $serialised ) {
-				return serialize( $data );
+				// @codingStandardsIgnoreLine
+				$data = serialize( $data );
+			}
+		} catch ( \Throwable $throwable ) {
+			if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+				throw $throwable;
 			}
 
-		} catch ( \Exception $error ) {
-
-			$this->add_error( $error->getMessage(), 'results' );
-
+			do_action( 'inpsyde.search_and_replace.error', $throwable );
 		}
 
 		return $data;
@@ -397,8 +417,8 @@ class Replace {
 		}
 		if ( ! empty( $input ) && is_string( $input ) ) {
 			return str_replace(
-				array( '\\', "\0", "\n", "\r", "'", '"', "\x1a" ),
-				array( '\\\\', '\\0', '\\n', '\\r', "\\'", '\\"', '\\Z' ),
+				[ '\\', "\0", "\n", "\r", "'", '"', "\x1a" ],
+				[ '\\\\', '\\0', '\\n', '\\r', "\\'", '\\"', '\\Z' ],
 				$input
 			);
 		}
--- tests/UnitTests/Database/ReplaceTest.php
+++ tests/UnitTests/Database/ReplaceTest.php
@@ -2,6 +2,8 @@
 
 use Inpsyde\SearchReplace\Database\Replace;
 use Inpsyde\SearchReplace\Tests\AbstractTestCase;
+use \Mockery as m;
+use \Brain\Monkey as bm;
 
 /**
  * Class ReplaceTest
@@ -11,211 +13,352 @@ class ReplaceTest extends AbstractTestCase {
 
 	function test_empty_search_string() {
 
-		$this->markTestIncomplete( 'This test has refactored.' );
-
-		$dbm    = $this->getMock( '\Inpsyde\SearchReplace\Database\Manager' );
-		$testee = new Replace( $dbm, $this->get_max_exec_mock() );
-
-		$result = $testee->replace_values( '', '', '' );
-		$this->assertContains( 'Search string is empty', $result );
-
-	}
-
-	function test_string_replace() {
-
-		$this->markTestIncomplete( 'This test has refactored.' );
-
-		$columns       = array(
-			0 => 'comment_ID',
-			1 =>
-				array(
-					0 => 'comment_ID',
-					1 => 'comment_post_ID',
-					2 => 'comment_author',
-
-				),
-		);
-		$table_content = array(
-			0 =>
-				array(
-					'comment_ID'      => '1',
-					'comment_post_ID' => '1',
-					'comment_author'  => 'Mr Wordpress',
-
-				),
-		);
-
-		$dbm_mock = $this->getMock(
-			'\Inpsyde\SearchReplace\Database\Manager',
-			array( 'get_columns', 'get_rows', 'get_table_content', 'flush' )
-		);
-
-		$dbm_mock->expects( $this->any() )
-			->method( 'get_columns' )
-			->will( $this->returnValue( $columns ) );
-
-		$dbm_mock->expects( $this->once() )
-			->method( 'get_rows' )
-			->will( $this->returnValue( 1 ) );
-
-		$dbm_mock->expects( $this->once() )
-			->method( 'get_table_content' )
-			->will( $this->returnValue( $table_content ) );
-
-		$dbm_mock->expects( $this->once() )
-			->method( 'flush' );
+		$dbm_mock = m::mock( '\Inpsyde\SearchReplace\Database\Manager' );
+		$dbm_mock->shouldReceive( 'get_table_structure' );
 
 		$testee = new Replace( $dbm_mock, $this->get_max_exec_mock() );
-		$result = $testee->replace_values( 'Mr Wordpress', 'Mr. Drupal', 'wp_plugin_test_comments' );
-		$this->assertEquals( $result[ 'changes' ][ 0 ][ 'to' ], 'Mr. Drupal' );
+		$result = $testee->replace_values( '', '', '' );
+
+		$this->assertContains( 'Search string is empty', $result );
 	}
 
+	/**
+	 * Pay attention to the code sniffer regarding the WordPress spell check.
+	 */
+	function test_string_replace() {
+
+		$columns = [
+			0 => 'comment_ID',
+			1 => [
+				0 => 'comment_ID',
+				1 => 'comment_post_ID',
+				2 => 'comment_author',
+			],
+		];
+
+		$table_content = [
+			0 => [
+				'comment_ID'      => '1',
+				'comment_post_ID' => '1',
+				'comment_author'  => 'Mr WordPress',
+			],
+		];
+
+		$table_structure = [
+			(object) [
+				'Field' => 'comment_author',
+				'Type'  => 'tinytext',
+			],
+		];
+
+		$dbm_mock = m::mock(
+			'\Inpsyde\SearchReplace\Database\Manager',
+			[ m::mock( '\wpdb' ) ]
+		);
+
+		$dbm_mock->shouldReceive( 'get_table_structure' )
+			->once()
+			->andReturn( $table_structure );
+
+		$dbm_mock->shouldReceive( 'get_columns' )
+			->andReturn( $columns );
+
+		$dbm_mock->shouldReceive( 'get_rows' )
+			->once()
+			->andReturn( 1 );
+
+		$dbm_mock->shouldReceive( 'get_table_content' )
+			->once()
+			->andReturn( $table_content );
+
+		$dbm_mock->shouldReceive( 'flush' )
+			->once();
+
+		$testee = new Replace( $dbm_mock, $this->get_max_exec_mock() );
+		$result = $testee->replace_values( 'Mr WordPress', 'Mr. Drupal', 'wp_plugin_test_comments' );
+
+		$this->assertEquals( 'Mr. Drupal', $result[ 'changes' ][ 0 ][ 'to' ] );
+	}
+
+	/**
+	 * Pay attention to the code sniffer regarding the WordPress spell check.
+	 */
 	function test_substring_replace() {
 
-		$this->markTestIncomplete( 'This test has refactored.' );
-
-		$columns       = array(
+		$columns = [
 			0 => 'comment_ID',
-			1 =>
-				array(
-					0 => 'comment_ID',
-					1 => 'comment_post_ID',
-					2 => 'comment_author',
+			1 => [
+				0 => 'comment_ID',
+				1 => 'comment_post_ID',
+				2 => 'comment_author',
+			],
+		];
 
-				),
+		$table_content = [
+			0 => [
+				'comment_ID'      => '1',
+				'comment_post_ID' => '1',
+				'comment_author'  => 'Mr WordPress',
+			],
+		];
+
+		$table_structure = [
+			(object) [
+				'Field' => 'comment_author',
+				'Type'  => 'tinytext',
+			],
+		];
+
+		$dbm_mock = m::mock(
+			'\Inpsyde\SearchReplace\Database\Manager',
+			[ m::mock( '\wpdb' ) ]
 		);
-		$table_content = array(
-			0 =>
-				array(
-					'comment_ID'      => '1',
-					'comment_post_ID' => '1',
-					'comment_author'  => 'Mr Wordpress',
 
-				),
-		);
+		$dbm_mock->shouldReceive( 'get_table_structure' )
+			->once()
+			->andReturn( $table_structure );
 
-		$dbm_mock = $this->getMock(
-			'\Inpsyde\searchReplace\inc\Database\Manager',
-			array( 'get_columns', 'get_rows', 'get_table_content', 'flush' )
-		);
+		$dbm_mock->shouldReceive( 'get_columns' )
+			->once()
+			->andReturn( $columns );
 
-		$dbm_mock->expects( $this->any() )
-			->method( 'get_columns' )
-			->will( $this->returnValue( $columns ) );
+		$dbm_mock->shouldReceive( 'get_rows' )
+			->once()
+			->andReturn( 1 );
 
-		$dbm_mock->expects( $this->once() )
-			->method( 'get_rows' )
-			->will( $this->returnValue( 1 ) );
+		$dbm_mock->shouldReceive( 'get_table_content' )
+			->once()
+			->andReturn( $table_content );
 
-		$dbm_mock->expects( $this->once() )
-			->method( 'get_table_content' )
-			->will( $this->returnValue( $table_content ) );
-
-		$dbm_mock->expects( $this->once() )
-			->method( 'flush' );
+		$dbm_mock->shouldReceive( 'flush' )
+			->once();
 
 		$testee = new Replace( $dbm_mock, $this->get_max_exec_mock() );
 		$result = $testee->replace_values( 'Mr', 'Mrs', 'wp_plugin_test_comments' );
-		$this->assertEquals( $result[ 'changes' ][ 0 ][ 'to' ], 'Mrs Wordpress' );
+
+		$this->assertEquals( $result[ 'changes' ][ 0 ][ 'to' ], 'Mrs WordPress' );
 	}
 
 	function test_object_replace() {
 
-		$this->markTestIncomplete( 'This test has refactored.' );
+		$serialized = 'a:1:{s:4:"Test";s:12:"Mr WordPress";}';
 
-		$columns       = array(
+		$columns = [
 			0 => 'comment_ID',
-			1 =>
-				array(
-					0 => 'comment_ID',
-					1 => 'comment_post_ID',
-					2 => 'comment_author',
+			1 => [
+				0 => 'comment_ID',
+				1 => 'comment_post_ID',
+				2 => 'comment_author',
+			],
+		];
 
-				),
+		$table_content = [
+			0 => [
+				'comment_ID'      => '1',
+				'comment_post_ID' => '1',
+				'comment_author'  => $serialized,
+			],
+		];
+
+		$table_structure = [
+			(object) [
+				'Field' => 'comment_author',
+				'Type'  => 'longtext',
+			],
+		];
+
+		bm\Functions\when( 'is_serialized_string' )
+			->alias(
+				function ( $data ) {
+
+					// if it isn't a string, it isn't a serialized string.
+					if ( ! is_string( $data ) ) {
+						return false;
+					}
+					$data = trim( $data );
+					if ( strlen( $data ) < 4 ) {
+						return false;
+					} elseif ( ':' !== $data[ 1 ] ) {
+						return false;
+					} elseif ( ';' !== substr( $data, - 1 ) ) {
+						return false;
+					} elseif ( $data[ 0 ] !== 's' ) {
+						return false;
+					} elseif ( '"' !== substr( $data, - 2, 1 ) ) {
+						return false;
+					} else {
+						return true;
+					}
+				}
+			);
+
+		bm\Functions\when( 'is_serialized' )
+			->alias(
+				function ( $data, $strict = true ) {
+
+					// if it isn't a string, it isn't serialized.
+					if ( ! is_string( $data ) ) {
+						return false;
+					}
+					$data = trim( $data );
+					if ( 'N;' == $data ) {
+						return true;
+					}
+					if ( strlen( $data ) < 4 ) {
+						return false;
+					}
+					if ( ':' !== $data[ 1 ] ) {
+						return false;
+					}
+					if ( $strict ) {
+						$lastc = substr( $data, - 1 );
+						if ( ';' !== $lastc && '}' !== $lastc ) {
+							return false;
+						}
+					} else {
+						$semicolon = strpos( $data, ';' );
+						$brace     = strpos( $data, '}' );
+						// Either ; or } must exist.
+						if ( false === $semicolon && false === $brace ) {
+							return false;
+						}
+						// But neither must be in the first X characters.
+						if ( false !== $semicolon && $semicolon < 3 ) {
+							return false;
+						}
+						if ( false !== $brace && $brace < 4 ) {
+							return false;
+						}
+					}
+					$token = $data[ 0 ];
+					switch ( $token ) {
+						case 's' :
+							if ( $strict ) {
+								if ( '"' !== substr( $data, - 2, 1 ) ) {
+									return false;
+								}
+							} elseif ( false === strpos( $data, '"' ) ) {
+								return false;
+							}
+						// or else fall through
+						case 'a' :
+						case 'O' :
+							return (bool) preg_match( "/^{$token}:[0-9]+:/s", $data );
+						case 'b' :
+						case 'i' :
+						case 'd' :
+							$end = $strict ? '$' : '';
+
+							return (bool) preg_match( "/^{$token}:[0-9.E-]+;$end/", $data );
+					}
+
+					return false;
+				}
+			);
+
+		bm\Functions\when( 'maybe_serialize' )
+			->alias(
+				function ( $data ) {
+
+					if ( is_array( $data ) || is_object( $data ) ) {
+						return serialize( $data );
+					}
+
+					// Double serialization is required for backward compatibility.
+					// See https://core.trac.wordpress.org/ticket/12930
+					// Also the world will end. See WP 3.6.1.
+					if ( is_serialized( $data, false ) ) {
+						return serialize( $data );
+					}
+
+					return $data;
+				}
+			);
+
+		$dbm_mock = m::mock(
+			'\Inpsyde\SearchReplace\Database\Manager',
+			[ m::mock( '\wpdb' ) ]
 		);
-		$table_content = array(
-			0 =>
-				array(
-					'comment_ID'      => '1',
-					'comment_post_ID' => '1',
-					'comment_author'  => 'a:1:{s:4:"Test";s:12:"Mr Wordpress";}',
 
-				),
-		);
+		$dbm_mock->shouldReceive( 'get_table_structure' )
+			->once()
+			->andReturn( $table_structure );
 
-		$dbm_mock = $this->getMock(
-			'\Inpsyde\searchReplace\inc\Database\Manager',
-			array( 'get_columns', 'get_rows', 'get_table_content', 'flush' )
-		);
+		$dbm_mock->shouldReceive( 'get_columns' )
+			->once()
+			->andReturn( $columns );
 
-		$dbm_mock->expects( $this->any() )
-			->method( 'get_columns' )
-			->will( $this->returnValue( $columns ) );
+		$dbm_mock->shouldReceive( 'get_rows' )
+			->once()
+			->andReturn( 1 );
 
-		$dbm_mock->expects( $this->once() )
-			->method( 'get_rows' )
-			->will( $this->returnValue( 1 ) );
+		$dbm_mock->shouldReceive( 'get_table_content' )
+			->once()
+			->andReturn( $table_content );
 
-		$dbm_mock->expects( $this->once() )
-			->method( 'get_table_content' )
-			->will( $this->returnValue( $table_content ) );
-
-		$dbm_mock->expects( $this->once() )
-			->method( 'flush' );
+		$dbm_mock->shouldReceive( 'flush' )
+			->once();
 
 		$testee = new Replace( $dbm_mock, $this->get_max_exec_mock() );
-		$result = $testee->replace_values( 'Mr Wordpress', 'Mr Drupal', 'wp_plugin_test_comments' );
-		$this->assertEquals( $result[ 'changes' ][ 0 ][ 'to' ], 'a:1:{s:4:"Test";s:9:"Mr Drupal";}' );
+		$result = $testee->replace_values( 'Mr WordPress', 'Mr Drupal', 'wp_plugin_test_comments' );
+
+		$this->assertEquals( 'a:1:{s:4:"Test";s:9:"Mr Drupal";}', $result[ 'changes' ][ 0 ][ 'to' ] );
 	}
 
 	function test_umlaut_replace() {
 
-		$this->markTestIncomplete( 'This test has refactored.' );
-
-		$columns       = array(
+		$columns = [
 			0 => 'comment_ID',
-			1 =>
-				array(
-					0 => 'comment_ID',
-					1 => 'comment_post_ID',
-					2 => 'comment_author',
+			1 => [
+				0 => 'comment_ID',
+				1 => 'comment_post_ID',
+				2 => 'comment_author',
+			],
+		];
 
-				),
+		$table_content = [
+			0 => [
+				'comment_ID'      => '1',
+				'comment_post_ID' => '1',
+				'comment_author'  => 'Mr Wordpress',
+			],
+		];
+
+		$table_structure = [
+			(object) [
+				'Field' => 'comment_author',
+				'Type'  => 'tinytext',
+			],
+		];
+
+		$dbm_mock = m::mock(
+			'\Inpsyde\SearchReplace\Database\Manager',
+			[ m::mock( '\wpdb' ) ]
 		);
-		$table_content = array(
-			0 =>
-				array(
-					'comment_ID'      => '1',
-					'comment_post_ID' => '1',
-					'comment_author'  => 'Mr Wordpress',
 
-				),
-		);
+		$dbm_mock->shouldReceive( 'get_table_structure' )
+			->once()
+			->andReturn( $table_structure );
 
-		$dbm_mock = $this->getMock(
-			'\Inpsyde\searchReplace\inc\Database\Manager',
-			array( 'get_columns', 'get_rows', 'get_table_content', 'flush' )
-		);
+		$dbm_mock->shouldReceive( 'get_columns' )
+			->once()
+			->andReturn( $columns );
 
-		$dbm_mock->expects( $this->any() )
-			->method( 'get_columns' )
-			->will( $this->returnValue( $columns ) );
+		$dbm_mock->shouldReceive( 'get_rows' )
+			->once()
+			->andReturn( 1 );
 
-		$dbm_mock->expects( $this->once() )
-			->method( 'get_rows' )
-			->will( $this->returnValue( 1 ) );
+		$dbm_mock->shouldReceive( 'get_table_content' )
+			->once()
+			->andReturn( $table_content );
 
-		$dbm_mock->expects( $this->once() )
-			->method( 'get_table_content' )
-			->will( $this->returnValue( $table_content ) );
-
-		$dbm_mock->expects( $this->once() )
-			->method( 'flush' );
+		$dbm_mock->shouldReceive( 'flush' )
+			->once();
 
 		$testee = new Replace( $dbm_mock, $this->get_max_exec_mock() );
 		$result = $testee->replace_values( 'Mr Wordpress', 'Mr. Drüpal', 'wp_plugin_test_comments' );
-		$this->assertEquals( $result[ 'changes' ][ 0 ][ 'to' ], 'Mr. Drüpal' );
-	}
 
+		$this->assertEquals( 'Mr. Drüpal', $result[ 'changes' ][ 0 ][ 'to' ] );
+	}
 
 }
