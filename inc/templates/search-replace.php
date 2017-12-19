a1a3bbfccb79b6a9ae1fdd5d2dc49c1c777a9dd4
Guido Scialfa <dev@guidoscialfa.com>
2017-12-19 13:20:14 +0100

    search and replace textarea for csv has the cursor not in the first column

--- inc/templates/search-replace.php
+++ inc/templates/search-replace.php
@@ -42,9 +42,7 @@ if ( ! defined( 'SEARCH_REPLACE_BASEDIR' ) ) {
 				<textarea id="csv" cols="46" rows="5" name="csv" placeholder="<?php esc_html_e(
 					'search value, replace value (one per line)',
 					'search-and-replace'
-				); ?>">
-					<?php $this->get_csv_value(); ?>
-				</textarea>
+				); ?>"><?php $this->get_csv_value(); ?></textarea>
 				<p id="csv-hint">
 					<?php esc_html_e(
 						'Using comma delimited( , ). For example to replace cat with dog: cat,dog',
